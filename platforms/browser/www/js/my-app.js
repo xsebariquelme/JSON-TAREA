c1f50137cd5d97ea6b823d98adffbe08910a5882
xsebariquelme <searique@alumnos.ubiobio.cl>
2017-06-10 19:50:52 -0400

    agregado los mensajes de saludo

--- platforms/browser/www/js/my-app.js
+++ platforms/browser/www/js/my-app.js
@@ -13,7 +13,10 @@ var mainView = myApp.addView('.view-main', {
 document.addEventListener('deviceready', function(){
     $('#sub').bind('click', enviar);
     $("#back").bind('click',back);
+    $('#santoral').bind('click',msgsanto);
 }, false);
+
+
 
 function open_login(){
     myApp.loginScreen();
@@ -42,19 +45,20 @@ function enviar(){
                   localStorage.setItem('santman',data.santoral.maniana);
                 if(user.toUpperCase()==data.santoral.hoy.toUpperCase()){
                      localStorage.setItem('saludo','Felicidades por estar de Santo');
+                        myApp.alert('Felicidades por estar de Santo', 'APP JSON');
                 }else{
                     if(user.toUpperCase()==data.santoral.ayer.toUpperCase()){
                      localStorage.setItem('saludo','Felicidades por haber estado de Santo ayer');
+                        myApp.alert('Felicidades por haber estado de Santo ayer', 'APP JSON');
                 }else{
                     if(user.toUpperCase()==data.santoral.maniana.toUpperCase()){
                      localStorage.setItem('saludo','Felicidades estarás de Santo mañana');
+                        myApp.alert('Felicidades estarás de Santo mañana','APP JSON');
                 }else{
                     localStorage.setItem('saludo', 'Tu nombre no coincide con los santos')
                 }
                 }
                 }
-              
-              
                   localStorage.setItem('dolar',data.moneda.dolar);
                   localStorage.setItem('euro',data.moneda.euro);
                   localStorage.setItem('uf',data.indicador.uf);
@@ -80,3 +84,15 @@ function enviar(){
    
 }
 
+function msgsanto(){
+     if(localStorage.getItem('user').toUpperCase()==localStorage.getItem('santhoy').toUpperCase()){
+                        myApp.alert('Felicidades por estar de Santo', 'APP JSON');
+                }
+    if(localStorage.getItem('user').toUpperCase()==localStorage.getItem('santayer').toUpperCase()){
+                        myApp.alert('Felicidades por haber estado de Santo ayer', 'APP JSON');
+     }
+                    if(localStorage.getItem('user').toUpperCase()==localStorage.getItem('santman').toUpperCase()){
+                        myApp.alert('Felicidades estarás de Santo mañana','APP JSON');
+                }
+                }
+                
